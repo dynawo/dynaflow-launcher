30bea0e70631c93de3cf1d3f2baf9c1aba869298
Le Courtois Florent <florent.lecourtois@rte-france.com>
2020-09-23 14:51:49 +0200

    Correct copyright

--- cmake/CheckVar.cmake
+++ cmake/CheckVar.cmake
@@ -1,3 +1,12 @@
+# Copyright (c) 2020, RTE (http://www.rte-france.com)
+# See AUTHORS.txt
+# All rights reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+#
+
 function(check_env_var _var)
   if (NOT DEFINED DYNAWO_HOME AND NOT DEFINED ENV{_var})
     message(FATAL_ERROR "Variable ${_var} is not defined")
--- cmake/Policy.cmake
+++ cmake/Policy.cmake
@@ -1,3 +1,12 @@
+# Copyright (c) 2015-2020, RTE (http://www.rte-france.com)
+# See AUTHORS.txt
+# All rights reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+#
+
 function(policy _policy _value)
   if (POLICY _policy)
     cmake_policy(SET _policy _value)
