439cd2ee644431fa4fa2272dc432e47a382777ad
Le Courtois Florent <florent.lecourtois@rte-france.com>
2020-09-24 11:22:18 +0200

    Correct optimization and debug info at compilation

---
+++ cmake/BuildType.cmake
@@ -0,0 +1,21 @@
+# Copyright (c) 2020, RTE (http://www.rte-france.com)
+# See AUTHORS.txt
+# All rights reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+#
+
+function(set_optimization)
+  set(CMAKE_CXX_FLAGS_DEBUG "-D_DEBUG_")
+  if (${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU" OR ${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
+    set(CMAKE_CXX_FLAGS_DEBUG "-g -O0 ${CMAKE_CXX_FLAGS_DEBUG}")
+    set(CMAKE_CXX_FLAGS_RELEASE "-O3 ${CMAKE_CXX_FLAGS_RELEASE}")
+  elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL "MSVC")
+    set(CMAKE_CXX_FLAGS_DEBUG "/O0 ${CMAKE_CXX_FLAGS_DEBUG}")
+    set(CMAKE_CXX_FLAGS_RELEASE "/O1 ${CMAKE_CXX_FLAGS_RELEASE}")
+  else()
+    message(WARNING "Unsupported compiler ${CMAKE_CXX_COMPILER_ID} : no debug info / optimization is performed")
+  endif()
+endfunction()
