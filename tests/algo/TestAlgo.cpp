85050e641d8fab2179ba84b32bcb7be1ad7bacb9
Le Courtois Florent <florent.lecourtois@rte-france.com>
2020-10-02 13:32:20 +0200

    Add test for same size components

--- tests/algo/TestAlgo.cpp
+++ tests/algo/TestAlgo.cpp
@@ -124,3 +124,31 @@ TEST(Connexity, base) {
   std::sort(nodeids_main.begin(), nodeids_main.end());
   ASSERT_EQ(expected_nodes, nodeids_main);
 }
+
+TEST(Connexity, SameSize) {
+  std::vector<std::shared_ptr<dfl::inputs::Node>> nodes{std::make_shared<dfl::inputs::Node>("0", 0.0), std::make_shared<dfl::inputs::Node>("1", 1.0),
+                                                        std::make_shared<dfl::inputs::Node>("2", 2.0), std::make_shared<dfl::inputs::Node>("3", 3.0),
+                                                        std::make_shared<dfl::inputs::Node>("4", 5.0), std::make_shared<dfl::inputs::Node>("5", 5.0)};
+  std::vector<dfl::inputs::Node::NodeId> expected_nodes{"0", "1", "2"};
+
+  nodes[0]->neighbours.push_back(nodes[1]);
+  nodes[0]->neighbours.push_back(nodes[2]);
+  nodes[1]->neighbours.push_back(nodes[0]);
+  nodes[2]->neighbours.push_back(nodes[0]);
+
+  nodes[3]->neighbours.push_back(nodes[4]);
+  nodes[3]->neighbours.push_back(nodes[5]);
+  nodes[4]->neighbours.push_back(nodes[3]);
+  nodes[5]->neighbours.push_back(nodes[3]);
+
+  dfl::algo::MainConnexComponentAlgorithm::ConnexGroup main;
+  dfl::algo::MainConnexComponentAlgorithm algo(main);
+
+  std::for_each(nodes.begin(), nodes.end(), algo);
+
+  ASSERT_EQ(3, main.size());
+  std::vector<dfl::inputs::Node::NodeId> nodeids_main;
+  std::for_each(main.begin(), main.end(), [&nodeids_main](const std::shared_ptr<dfl::inputs::Node>& node) { nodeids_main.push_back(node->id); });
+  std::sort(nodeids_main.begin(), nodeids_main.end());
+  ASSERT_EQ(expected_nodes, nodeids_main);
+}
