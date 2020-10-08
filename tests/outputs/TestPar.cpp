7714c9dbefefc4836b7794b0573858fcf5f809b4
Le Courtois Florent <florent.lecourtois@rte-france.com>
2020-10-08 08:34:10 +0200

    Add unit test for Dyd and Par writer

---
+++ tests/outputs/TestPar.cpp
@@ -0,0 +1,66 @@
+//
+// Copyright (c) 2020, RTE (http://www.rte-france.com)
+// See AUTHORS.txt
+// All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, you can obtain one at http://mozilla.org/MPL/2.0/.
+// SPDX-License-Identifier: MPL-2.0
+//
+
+#include "Par.h"
+#include "Tests.h"
+
+#include <boost/filesystem.hpp>
+
+TEST(TestPar, write) {
+  using dfl::algo::GeneratorDefinition;
+  using dfl::algo::LoadDefinition;
+
+  std::string basename = "TestPar";
+  std::string outputDir = "results/" + basename;
+  std::string filename = basename + ".par";
+
+  if (!boost::filesystem::exists(outputDir)) {
+    boost::filesystem::create_directories(outputDir);
+  }
+
+  std::vector<GeneratorDefinition> generators = {GeneratorDefinition("G0", GeneratorDefinition::ModelType::SIGNALN, "00",
+                                                                     {
+                                                                         GeneratorDefinition::ReactiveCurvePoint(1., 11., 110.),
+                                                                         GeneratorDefinition::ReactiveCurvePoint(2., 22., 220.),
+                                                                         GeneratorDefinition::ReactiveCurvePoint(3., 33., 330.),
+                                                                         GeneratorDefinition::ReactiveCurvePoint(4., 44., 440.),
+                                                                     },
+                                                                     1., 10., 11., 110.),
+                                                 GeneratorDefinition("G1", GeneratorDefinition::ModelType::WITH_IMPEDANCE_SIGNALN, "01",
+                                                                     {
+                                                                         GeneratorDefinition::ReactiveCurvePoint(1., 11., 110.),
+                                                                         GeneratorDefinition::ReactiveCurvePoint(2., 22., 220.),
+                                                                         GeneratorDefinition::ReactiveCurvePoint(3., 33., 330.),
+                                                                         GeneratorDefinition::ReactiveCurvePoint(4., 44., 440.),
+                                                                     },
+                                                                     2., 20., 22., 220.),
+                                                 GeneratorDefinition("G2", GeneratorDefinition::ModelType::DIAGRAM_PQ_SIGNALN, "02",
+                                                                     {
+                                                                         GeneratorDefinition::ReactiveCurvePoint(1., 11., 110.),
+                                                                         GeneratorDefinition::ReactiveCurvePoint(2., 22., 220.),
+                                                                         GeneratorDefinition::ReactiveCurvePoint(3., 33., 330.),
+                                                                         GeneratorDefinition::ReactiveCurvePoint(4., 44., 440.),
+                                                                     },
+                                                                     3., 30., 33., 330.),
+                                                 GeneratorDefinition("G3", GeneratorDefinition::ModelType::WITH_IMPEDANCE_DIAGRAM_PQ_SIGNALN, "03",
+                                                                     {
+                                                                         GeneratorDefinition::ReactiveCurvePoint(1., 11., 110.),
+                                                                         GeneratorDefinition::ReactiveCurvePoint(2., 22., 220.),
+                                                                         GeneratorDefinition::ReactiveCurvePoint(3., 33., 330.),
+                                                                         GeneratorDefinition::ReactiveCurvePoint(4., 44., 440.),
+                                                                     },
+                                                                     4., 40., 44., 440.)};
+
+  dfl::outputs::Par parWriter(dfl::outputs::Par::ParDefinition(basename, outputDir + "/" + filename, generators));
+
+  parWriter.write();
+
+  dfl::test::checkFilesEqual(outputDir + "/" + filename, "reference/" + basename + "/" + filename);
+}
