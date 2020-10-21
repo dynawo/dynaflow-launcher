efba2ebe5e0a0e1d01b77ef924aea786d39c5134
Le Courtois Florent <florent.lecourtois@rte-france.com>
2020-10-21 11:29:07 +0200

    Add possibility to compile in shared

--- scripts/dynaflow-launcher.sh
+++ scripts/dynaflow-launcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 INSTALL=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 export DYNAWO_HOME=$INSTALL/external/dynawo
-export LD_LIBRARY_PATH=$DYNAWO_HOME/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$INSTALL/lib64:$DYNAWO_HOME/lib:$LD_LIBRARY_PATH
 export IIDM_XML_XSD_PATH=$DYNAWO_HOME/share/iidm/xsd
 export DYNAFLOW_LAUNCHER_LOCALE=en_GB
 export DYNAFLOW_LAUNCHER_INSTALL=$INSTALL
