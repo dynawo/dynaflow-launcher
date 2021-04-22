e299cdb654d4d448329da59767aecb2dcd45f89a
Le Courtois Florent <florent.lecourtois@rte-france.com>
2021-04-22 11:02:29 +0200

    #140 Add sript to compile and launch DFL

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Dynawflow Launcher (DFL) depends on the [core dynawo libraries of Dyna&omega;o](https://github.com/dynawo/dynawo)
 
 ## Build
-To build DynaFlow launcher, you must first deploy the Dyna&omega;o library with *c++11 enabled*.
+To build DynaFlow launcher, you must first deploy the Dyna&omega;o library with **c++11 enabled**.
 
 ``` bash
 $> ./myEnvDynawo.sh build-all
@@ -17,9 +17,50 @@ The path to dynawo deploy is then the path to the subdirectory `dynawo` in the d
 PATH_TO_DYNAWO_DEPLOY=${DYNAWO_HOME}/deploy/gcc8/shared/dynawo/
 ```
 
-Then generate the cmake cache in your build directory:
+### By script
 
-`cmake <SRC_DIR> -DCMAKE_BUILD_TYPE=Release -DDYNAWO_HOME=<DYNAWO_HOME> -DBOOST_ROOT=<DYNAWO_HOME> -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>`
+To build DynaFlow Launcher you need to clone the repository and launch the following commands in the source code directory, it will create a `myEnvDFL.sh` file that will be your personal entrypoint to launch DFL and parametrise some options.
+
+``` bash
+$> git clone https://github.com/dynawo/dynaflow-launcher
+$> cd dynaflow-launcher
+$> echo '#!/bin/bash
+
+# Required
+export DYNAFLOW_LAUNCHER_HOME=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+export DYNAWO_HOME=<PATH_TO_DYNAWO_DEPLOY>
+export DYNAFLOW_LAUNCHER_BUILD_TYPE=Debug
+export DYNAFLOW_LAUNCHER_LOCALE=en_GB
+
+# Optional
+# export DYNAFLOW_LAUNCHER_CMAKE_GENERATOR=Ninja # default is Unix Makefiles
+# export DYNAFLOW_LAUNCHER_PROCESSORS_USED=8 # default 1
+# export DYNAFLOW_LAUNCHER_SHARED_LIB=ON # default OFF
+# export DYNAFLOW_LAUNCHER_USE_DOXYGEN=OFF # default ON
+# export DYNAFLOW_LAUNCHER_BUILD_TESTS=OFF # default ON
+# export DYNAFLOW_LAUNCHER_LOG_LEVEL=INFO # default INFO: can be DEBUG, INFO, WARN, ERROR
+
+# Run
+$DYNAFLOW_LAUNCHER_HOME/scripts/envDFL.sh $@' > myEnvDFL.sh
+$> chmod +x myEnvDFL.sh
+```
+
+Then update the path "PATH_TO_DYNAWO_DEPLOY" in the file to your deployed installation of Dyna&omega;o and launch the following command:
+``` bash
+$> ./myEnvDFL.sh build-user
+```
+
+All commands described in the rest of this README are accessible throught this script. To access all options of the script, type:
+``` bash
+$> ./myEnvDFL.sh help
+```
+
+### Direct CMake compilation
+You can generate the cmake cache in your build directory without using the script. Go to your build directory and then launch:
+
+```bash
+$> cmake <SRC_DIR> -DCMAKE_BUILD_TYPE=Release -DDYNAWO_HOME=<DYNAWO_HOME> -DBOOST_ROOT=<DYNAWO_HOME> -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+```
 
 where:
 * SRC_DIR is the root directory of the dynaflow launcher repository
@@ -35,30 +76,45 @@ The following **optional** CMake variables can be added:
 
 Then build the project in the build directory:
 
-`cmake --build . --target install`
+```bash
+$> cmake --build . --target install
+```
 
 ## Run
 To run DynaFlow launcher, use the script dynaflow-launcher.sh provided with the installation. This script will set the required environment variables.
 
 Runtime options of binary are given by doing:
 
-`dynaflow-launcher.sh --help`
+```bash
+$> ./dynaflow-launcher.sh --help
+```
+from the install directlory
 
-Only network file and configuration are required
+Only network file and configuration are required.
+
+You can also use the script with the "launch" or "launch-dir" option.
 
 ## Testing
 DynaFlow Launcher testing relies on cmake tests. To launch units tests, launch from *build* directory:
 
-`ctest -j<N> --output-on-failure`
+```bash
+$> ctest -j<N> --output-on-failure
+```
 
-where N is the number of jobs to use for the testing session
+where N is the number of jobs to use for the testing session.
+
+You can also use the script with the "tests" option.
 
 ## Coverage
 Coverage analysis is based on ctest. To launch coverage, launch from source directory:
 
-`ctest -S cmake/CTestScript.cmake -DDYNAWO_HOME=<DYNAWO_HOME> -DBOOST_ROOT=<DYNAWO_HOME> -VV`
+```bash
+$> ctest -S cmake/CTestScript.cmake -DDYNAWO_HOME=<DYNAWO_HOME> -DBOOST_ROOT=<DYNAWO_HOME> -VV
+```
 
-Outputs are gcov and lcov reports
+Outputs are gcov and lcov reports.
+
+You can also use the script with the "build-tests-coverage" option.
 
 ## Developement standard
 ### Check style
@@ -68,4 +124,8 @@ The check style, based on Dynawo's, is based on a [clang-format file](https://cl
 git commits have a standard pattern, similar to the one in Dynawo core. This pattern can be checked by hooks. In order to install this hook, the
 developper must run the script:
 
-`./scripts/set_commit_hook.sh`
+```bash
+$> ./scripts/set_commit_hook.sh
+```
+
+This is done automatically when using the script with any option.
---
+++ scripts/envDFL.sh
@@ -0,0 +1,243 @@
+#!/bin/bash
+#
+# Copyright (c) 2021, RTE (http://www.rte-france.com)
+# See AUTHORS.txt
+# All rights reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+#
+
+# This file aimes to encapsulate processing for users to compiler and use DFL
+
+HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+error_exit() {
+    echo "${1:-"Unknown Error"}" 1>&2
+    exit 1
+}
+
+export_var_env() {
+    local var="$@"
+    local name=${var%%=*}
+    local value="${var#*=}"
+
+    if ! `expr $name : "DYNAFLOW_LAUNCHER_.*" > /dev/null`; then
+      error_exit "You must export variables with DYNAWO prefix for $name."
+    fi
+
+    if eval "[ \"\$$name\" ]"; then
+      eval "value=\${$name}"
+      ##echo "Environment variable for $name already set : $value"
+      return
+    fi
+
+    if [ "$value" = UNDEFINED ]; then
+      error_exit "You must define the value of $name"
+    fi
+    export $name="$value"
+}
+
+ld_library_path_remove() {
+    export LD_LIBRARY_PATH=`echo -n $LD_LIBRARY_PATH | awk -v RS=: -v ORS=: '$0 != "'$1'"' | sed 's/:$//'`;
+}
+
+define_options() {
+    export_var_env DYNAFLOW_LAUNCHER_USAGE="Usage: `basename $0` [option] -- program to deal with DynaFlow Launcher
+
+where [option] can be:
+        =========== Build
+        build-user                              build DynaFlow Launcher
+        build-tests-coverage                    build DynaFlow Launcher and run coverage
+
+        =========== Tests
+        tests                                   launch DynaFlow Launcher unit tests
+
+        =========== Launch
+        launch-dir [dir]                        launch DynaFlow Launcher with:
+                                                - network filepath = <dir>/network.xml (only IIDM is supported)
+                                                - configuration filepath = <dir>/config.json
+        launch [network] [config]               launch DynaFlow Launcher
+
+        =========== Others
+        help                                    show all available options
+        version                                 show DynaFlow Launcher version
+        reset-environment                       reset all environment variables set by DynaFlow Launcher
+"
+}
+
+help() {
+    define_options
+    echo "$DYNAFLOW_LAUNCHER_USAGE"
+}
+
+set_commit_hook() {
+    $HERE/set_commit_hook.sh
+}
+
+set_environment() {
+    # dynawo vars
+    export DYNAWO_INSTALL_DIR=$DYNAWO_HOME
+    export LD_LIBRARY_PATH=$DYNAWO_INSTALL_DIR/lib:$LD_LIBRARY_PATH
+    export IIDM_XML_XSD_PATH=$DYNAWO_INSTALL_DIR/share/iidm/xsd
+    export DYNAWO_RESOURCES_DIR=$DYNAWO_INSTALL_DIR/share:$DYNAWO_INSTALL_DIR/share/xsd
+    export DYNAWO_DDB_DIR=$DYNAWO_INSTALL_DIR/ddb
+    export DYNAWO_IIDM_EXTENSION=$DYNAWO_INSTALL_DIR/lib/libdynawo_DataInterfaceIIDMExtension.so
+    export DYNAWO_LIBIIDM_EXTENSIONS=$DYNAWO_INSTALL_DIR/lib
+
+    # build
+    export DYNAFLOW_LAUNCHER_BUILD_DIR=$DYNAFLOW_LAUNCHER_HOME/buildLinux
+    export DYNAFLOW_LAUNCHER_INSTALL_DIR=$DYNAFLOW_LAUNCHER_HOME/installLinux
+    export_var_env DYNAFLOW_LAUNCHER_SHARED_LIB=OFF # same default value as cmakelist
+    export_var_env DYNAFLOW_LAUNCHER_USE_DOXYGEN=ON # same default value as cmakelist
+    export_var_env DYNAFLOW_LAUNCHER_BUILD_TESTS=ON # same default value as cmakelist
+    export_var_env DYNAFLOW_LAUNCHER_CMAKE_GENERATOR="Unix Makefiles"
+    export_var_env DYNAFLOW_LAUNCHER_PROCESSORS_USED=1
+
+    # Run
+    export DYNAFLOW_LAUNCHER_INSTALL=$DYNAFLOW_LAUNCHER_INSTALL_DIR
+    export LD_LIBRARY_PATH=$DYNAFLOW_LAUNCHER_HOME/lib64:$LD_LIBRARY_PATH
+    export_var_env DYNAFLOW_LAUNCHER_LOG_LEVEL=INFO # INFO by default
+
+    # other
+    set_commit_hook
+}
+
+reset_environment_variables() {
+    for var in $(printenv | grep DYNAFLOW_LAUNCHER_ | cut -d '=' -f 1); do
+        unset $var
+    done
+
+    ld_library_path_remove $DYNAFLOW_LAUNCHER_HOME/lib64
+    ld_library_path_remove $DYNAFLOW_LAUNCHER_HOME/lib
+
+    unset DYNAWO_HOME
+    unset DYNAWO_INSTALL_DIR
+    unset DYNAWO_RESOURCES_DIR
+    unset DYNAWO_DDB_DIR
+    unset DYNAWO_IIDM_EXTENSION
+    unset DYNAWO_LIBIIDM_EXTENSIONS
+    unset IIDM_XML_XSD_PATH
+}
+
+clean() {
+    rm -rf $DYNAFLOW_LAUNCHER_BUILD_DIR
+    rm -rf $DYNAFLOW_LAUNCHER_INSTALL_DIR
+}
+
+cmake_configure() {
+    cmake \
+        -S $DYNAFLOW_LAUNCHER_HOME \
+        -B $DYNAFLOW_LAUNCHER_BUILD_DIR \
+        -G $DYNAFLOW_LAUNCHER_CMAKE_GENERATOR \
+        -DCMAKE_BUILD_TYPE:STRING=$DYNAFLOW_LAUNCHER_BUILD_TYPE \
+        -DCMAKE_INSTALL_PREFIX:STRING=$DYNAFLOW_LAUNCHER_INSTALL_DIR \
+        -DDYNAWO_HOME:STRING=$DYNAWO_HOME \
+        -DBOOST_ROOT:STRING=$DYNAWO_HOME \
+        -DDYNAFLOW_LAUNCHER_LOCALE:STRING=$DYNAFLOW_LAUNCHER_LOCALE \
+        -DDYNAFLOW_LAUNCHER_SHARED_LIB:BOOL=$DYNAFLOW_LAUNCHER_SHARED_LIB \
+        -DDYNAFLOW_LAUNCHER_USE_DOXYGEN:BOOL=$DYNAFLOW_LAUNCHER_USE_DOXYGEN \
+        -DDYNAFLOW_LAUNCHER_BUILD_TESTS:BOOL=$DYNAFLOW_LAUNCHER_BUILD_TESTS
+}
+
+cmake_build() {
+    cmake --build $DYNAFLOW_LAUNCHER_BUILD_DIR --target install -j $DYNAFLOW_LAUNCHER_PROCESSORS_USED
+}
+
+cmake_tests() {
+    pushd $DYNAFLOW_LAUNCHER_BUILD_DIR
+    ctest -j $DYNAFLOW_LAUNCHER_PROCESSORS_USED --output-on-failure
+    popd
+}
+
+cmake_coverage() {
+    ctest \
+        -S cmake/CTestScript.cmake \
+        -DDYNAWO_HOME=$DYNAWO_HOME \
+        -DBOOST_ROOT=$DYNAWO_HOME \
+        -VV
+}
+
+clean_build_all() {
+    clean
+    build_user
+}
+
+build_user() {
+    cmake_configure
+    cmake_build
+}
+
+build_tests_coverage() {
+    rm -rf $DYNAFLOW_LAUNCHER_HOME/buildCoverage
+    cmake_coverage
+}
+
+launch() {
+    $DYNAFLOW_LAUNCHER_INSTALL_DIR/bin/DynaFlowLauncher \
+    --log-level $DYNAFLOW_LAUNCHER_LOG_LEVEL \
+    --network $1 \
+    --config $2
+}
+
+version() {
+    $DYNAFLOW_LAUNCHER_INSTALL_DIR/bin/DynaFlowLauncher --version
+}
+
+launch_dir() {
+    network=$1/network.xml
+    if [ ! -f $network ]; then
+        error_exit "IIDM network file $network doesn't exist"
+    fi
+
+    config=$1/config.json
+    if [ ! -f $config ]; then
+        error_exit "DFL configuration file $config doesn't exist"
+    fi
+
+    launch $network $config
+}
+
+#################################
+########### Main script #########
+#################################
+
+CMD=$1
+
+set_environment
+
+case $1 in
+    clean-build-all)
+        clean_build_all || error_exit "Failed to clean build DFL"
+        ;;
+    build-user)
+        build_user || error_exit "Failed to build DFL"
+        ;;
+    build-tests-coverage)
+        build_tests_coverage || error_exit "Failed to perform coverage"
+        ;;
+    tests)
+        cmake_tests || error_exit "Failed to perform tests"
+        ;;
+    launch-dir)
+        launch_dir $2 || error_exit "Failed to perform launch with dir=$2"
+        ;;
+    launch)
+        launch $2 $3 || error_exit "Failed to perform launch with network=$2, config=$3"
+        ;;
+    help)
+        help
+        ;;
+    version)
+        version
+        ;;
+    reset-environment)
+        reset_environment_variables || error_exit "Failed to reset environment variables"
+        ;;
+    *)
+        echo "$1 is an invalid option"
+        help
+        exit 1
+        ;;
+esac
