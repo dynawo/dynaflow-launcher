faede67cb4498a3edaf230cc48cfbf1af9c3eacd
Luma <zamarrenolm@aia.es>
2021-05-05 17:05:47 +0200

    #132 specific jobs,dyd,par for contingency and run simulations

---
+++ sources/Outputs/include/ParEvent.h
@@ -0,0 +1,96 @@
+//
+// Copyright (c) 2021, RTE (http://www.rte-france.com)
+// See AUTHORS.txt
+// All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, you can obtain one at http://mozilla.org/MPL/2.0/.
+// SPDX-License-Identifier: MPL-2.0
+//
+
+/**
+ * @file  ParEvent.h
+ *
+ * @brief Dynaflow launcher PAR file writer for events in a contingency header file
+ *
+ */
+
+#pragma once
+
+#include "Algo.h"
+#include "Configuration.h"
+
+#include <PARParametersSet.h>
+#include <boost/shared_ptr.hpp>
+#include <functional>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace dfl {
+namespace outputs {
+/**
+ * @brief PAR event file writer
+ */
+class ParEvent {
+ public:
+  /**
+   * @brief PAR event file definition
+   */
+  struct ParEventDefinition {
+    /**
+     * @brief Constructor
+     *
+     * @param base basename for current simulation
+     * @param filename file path for output PAR file (corresponds to basename)
+     * // TODO(Luma) should be a list of (elementId, elementType)
+     * @param elementId element identifier of the equipment affected by the contingency
+     * @param elementType element identifier of the equipment affected by the contingency
+     * @param timeEvent time of event
+     */
+    ParEventDefinition(const std::string& base, const std::string& filename,
+            const std::string& elementId,
+            const std::string& elementType,
+            const double& timeEvent) :
+        basename(base),
+        filename(filename),
+        elementId(elementId),
+        elementType(elementType),
+        timeEvent(timeEvent) {}
+
+    std::string basename;     ///< basename
+    std::string filename;     ///< filename of the output file to write
+    std::string elementId;    ///< Identifier of the equipment affected by the contingency
+    std::string elementType;  ///< Type of the equipment affected by the contingency
+    double timeEvent;         ///< Time of event
+  };
+
+  /**
+   * @brief Constructor
+   *
+   * @param def PAR event file definition
+   */
+  explicit ParEvent(ParEventDefinition&& def);
+
+  /**
+   * @brief Export PAR event file
+   */
+  void write();
+
+ private:
+  /**
+   * @brief Write branch disconnection parameter set
+   *
+   * @param branchId the identifier of the branch
+   * @param timeEvent time of event
+   *
+   * @returns the parameter set
+   */
+  static boost::shared_ptr<parameters::ParametersSet> writeBranchDisconnection(const std::string& branchId, double timeEvent);
+
+ private:
+  ParEventDefinition def_;  ///< PAR event file definition
+};
+
+}  // namespace outputs
+}  // namespace dfl
