6fa52736eb16f0edfddb607a6fa60ea843d47deb
Gautier Bureau <gautier.bureau@rte-france.com>
2022-05-13 08:44:02 +0200

    #381 Fix find_package gmock to handle debug libraries.

--- cmake/FindGMock.cmake
+++ cmake/FindGMock.cmake
@@ -11,25 +11,28 @@
 #
 #  GMOCK_INCLUDE_DIR - where to find gmock.h
 #  GMOCK_FOUND       - True if gmock found.
-IF(NOT GMOCK_HOME AND NOT $ENV{GMOCK_HOME} STREQUAL "")
-  SET(GMOCK_HOME $ENV{GMOCK_HOME})
-ENDIF()
+if(NOT GMOCK_HOME AND NOT $ENV{GMOCK_HOME} STREQUAL "")
+  set(GMOCK_HOME $ENV{GMOCK_HOME})
+endif()
 
-IF(NOT GMOCK_HOME AND NOT $ENV{GMOCK_ROOT} STREQUAL "")
-  SET(GMOCK_HOME $ENV{GMOCK_ROOT})
-ENDIF()
+if(NOT GMOCK_HOME AND NOT $ENV{GMOCK_ROOT} STREQUAL "")
+  set(GMOCK_HOME $ENV{GMOCK_ROOT})
+endif()
 
-FIND_PATH(GMOCK_INCLUDE_DIR NAME gmock/gmock.h gtest/gtest.h HINTS $ENV{GMOCK_HOME}/include ${GMOCK_HOME}/include)
-FIND_LIBRARY(GMOCK_LIBRARY NAME gmock libgmock gtest libgtest HINTS ENV{GMOCK_HOME} ${GMOCK_HOME} PATH_SUFFIXES lib lib${CMAKE_LIBRARY_ARCHITECTURE} )
-FIND_LIBRARY(GMOCK_MAIN_LIBRARY NAME gmock_main liggmock_main gtest_main libgtest_main
+find_path(GMOCK_INCLUDE_DIR NAME gmock/gmock.h gtest/gtest.h HINTS $ENV{GMOCK_HOME}/include ${GMOCK_HOME}/include)
+find_library(GMOCK_LIBRARY NAME gmock libgmock gtest libgtest HINTS ENV{GMOCK_HOME} ${GMOCK_HOME} PATH_SUFFIXES lib lib${CMAKE_LIBRARY_ARCHITECTURE} )
+find_library(GMOCK_MAIN_LIBRARY NAME gmock_main liggmock_main gtest_main libgtest_main
              HINTS ENV{GMOCK_HOME} ${GMOCK_HOME} PATH_SUFFIXES lib lib${CMAKE_LIBRARY_ARCHITECTURE} )
+find_library(GMOCK_LIBRARY_DEBUG NAME gmockd libgmockd gtestd libgtestd HINTS ENV{GMOCK_HOME} ${GMOCK_HOME} PATH_SUFFIXES lib lib${CMAKE_LIBRARY_ARCHITECTURE} )
+find_library(GMOCK_MAIN_LIBRARY_DEBUG NAME gmock_maind liggmock_maind gtest_maind libgtest_main
+  HINTS ENV{GMOCK_HOME} ${GMOCK_HOME} PATH_SUFFIXES lib lib${CMAKE_LIBRARY_ARCHITECTURE} )
 
-MARK_AS_ADVANCED(GMOCK_INCLUDE_DIR GMOCK_LIBRARY GMOCK_MAIN_LIBRARY)
+mark_as_advanced(GMOCK_INCLUDE_DIR GMOCK_LIBRARY GMOCK_MAIN_LIBRARY)
 
 # Handle the QUIETLY and REQUIRED arguments and set GMOCK_FOUND
 # to TRUE if all listed variables are TRUE.
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(GMock DEFAULT_MSG GMOCK_INCLUDE_DIR GMOCK_LIBRARY GMOCK_MAIN_LIBRARY)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(GMock DEFAULT_MSG GMOCK_INCLUDE_DIR GMOCK_LIBRARY GMOCK_MAIN_LIBRARY)
 
 if(GMOCK_FOUND)
   set(GMOCK_INCLUDE_DIRS ${GMOCK_INCLUDE_DIR})
@@ -42,7 +45,12 @@ if(GMOCK_FOUND)
       set_target_properties(GTest::gmock PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES "${GMOCK_INCLUDE_DIRS}")
     endif()
-    if(EXISTS "${GMOCK_LIBRARY}")
+    if(EXISTS "${GMOCK_LIBRARY_DEBUG}")
+      set_target_properties(GTest::gmock PROPERTIES
+        IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
+        IMPORTED_LINK_INTERFACE_LIBRARIES "Threads::Threads;GTest::gtest"
+        IMPORTED_LOCATION "${GMOCK_LIBRARY_DEBUG}")
+    elseif(EXISTS "${GMOCK_LIBRARY}")
       set_target_properties(GTest::gmock PROPERTIES
         IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
         IMPORTED_LINK_INTERFACE_LIBRARIES "Threads::Threads;GTest::gtest"
@@ -56,11 +64,16 @@ if(GMOCK_FOUND)
       set_target_properties(GTest::gmock_main PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES "${GMOCK_INCLUDE_DIRS}")
     endif()
-    if(EXISTS "${GMOCK_MAIN_LIBRARY}")
+    if(EXISTS "${GMOCK_MAIN_LIBRARY_DEBUG}")
       set_target_properties(GTest::gmock_main PROPERTIES
         IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
         IMPORTED_LINK_INTERFACE_LIBRARIES "Threads::Threads;GTest::gmock"
-        IMPORTED_LOCATION "${GMOCK_MAIN_LIBRARY}")
+        IMPORTED_LOCATION "${GMOCK_MAIN_LIBRARY_DEBUG}")
+    elseif(EXISTS "${GMOCK_MAIN_LIBRARY}")
+        set_target_properties(GTest::gmock_main PROPERTIES
+          IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
+          IMPORTED_LINK_INTERFACE_LIBRARIES "Threads::Threads;GTest::gmock"
+          IMPORTED_LOCATION "${GMOCK_MAIN_LIBRARY}")
     endif()
   endif()
 endif()
