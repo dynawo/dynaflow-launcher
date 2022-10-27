a0d9c1a88d1a7ec7d5a5e30dadb66e577f06e787
rosiereflo <florentine.rosiere@rte-france.com>
2022-10-27 13:35:38 +0200

    #461 fixes after review

--- sources/Inputs/include/Configuration.h
+++ sources/Inputs/include/Configuration.h
@@ -242,7 +242,7 @@ class Configuration {
   double timeStep_ = 10.;                                                            ///< maximum value of the solver timestep
   double timeOfEvent_ = 10.;                                                         ///< time for contingency simulation (security analysis only)
   std::unordered_set<ChosenOutputEnum, ChosenOutputHash> chosenOutputs_;             ///< chosen configuration outputs
-  double tfoVoltageLevel_ = 100;  ///< Maximum voltage level we assume that generator's transformers are already described in the static description
+  double tfoVoltageLevel_ = 100;                                                     ///< Maximum voltage level we assume that generator's transformers are already described in the static description
 };
 
 }  // namespace inputs
