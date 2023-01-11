3e30d6fd7257a966aca3feb2097ce2df3f03281f
CHIARAMELLO Marco <marco.chiaramello@rte-france.com>
2023-01-11 09:31:45 +0100

    #526 fix following review

--- sources/Outputs/src/DydGenerator.cpp
+++ sources/Outputs/src/DydGenerator.cpp
@@ -109,7 +109,7 @@ DydGenerator::writeSignalNBlackBox(boost::shared_ptr<dynamicdata::DynamicModelsC
 
 void
 DydGenerator::writeMacroConnect(boost::shared_ptr<dynamicdata::DynamicModelsCollection>& dynamicModelsToConnect) {
-  std::unordered_map<std::string, int> ModelNQIdGenNumber;
+  std::unordered_map<std::string, unsigned int> modelNQIdGenNumber;
   for (auto it = generatorDefinitions_.cbegin(); it != generatorDefinitions_.cend(); ++it) {
     if (it->isNetwork()) {
       continue;
@@ -121,11 +121,11 @@ DydGenerator::writeMacroConnect(boost::shared_ptr<dynamicdata::DynamicModelsColl
     } else if (it->model == algo::GeneratorDefinition::ModelType::PROP_SIGNALN_INFINITE ||
                it->model == algo::GeneratorDefinition::ModelType::PROP_DIAGRAM_PQ_SIGNALN ||
                it->model == algo::GeneratorDefinition::ModelType::PROP_SIGNALN_RECTANGULAR) {
-      std::string ModelNQId = constants::modelSignalNQprefix_ + it->regulatedBusId;
-      ModelNQIdGenNumber[ModelNQId]++;
-      dynamicModelsToConnect->addConnect(it->id, "generator_NQ", ModelNQId, "vrremote_NQ");
-      dynamicModelsToConnect->addConnect(it->id, "generator_limUQUp", ModelNQId, "vrremote_limUQUp_" + std::to_string(ModelNQIdGenNumber[ModelNQId]) + "_");
-      dynamicModelsToConnect->addConnect(it->id, "generator_limUQDown", ModelNQId, "vrremote_limUQDown_" + std::to_string(ModelNQIdGenNumber[ModelNQId]) + "_");
+      std::string modelNQId = constants::modelSignalNQprefix_ + it->regulatedBusId;
+      ++modelNQIdGenNumber[modelNQId];
+      dynamicModelsToConnect->addConnect(it->id, "generator_NQ", modelNQId, "vrremote_NQ");
+      dynamicModelsToConnect->addConnect(it->id, "generator_limUQUp", modelNQId, "vrremote_limUQUp_" + std::to_string(modelNQIdGenNumber[modelNQId]) + "_");
+      dynamicModelsToConnect->addConnect(it->id, "generator_limUQDown", modelNQId, "vrremote_limUQDown_" + std::to_string(modelNQIdGenNumber[modelNQId]) + "_");
     }
 
     auto connection = dynamicdata::MacroConnectFactory::newMacroConnect(macroConnectorGenName_, it->id, constants::networkModelName);
