de5302382d3684d95973ff90f052a7f2421a5508
Gautier Bureau <gautier.bureau@rte-france.com>
2023-01-18 14:37:12 +0100

    #539 Update api to use token in workflow and avoid github limite rate.

--- .github/workflows/tools.sh
+++ .github/workflows/tools.sh
@@ -11,7 +11,7 @@ wait_for_http_code()
     while [[ (-z "$CODE" || "$CODE" -ne "$WAIT_CODE") && ("$RETRIES" -lt "$MAX_RETRIES") ]]
     do
       sleep 1
-      CODE=$(curl --silent --header "Authorization: token $TOKEN" --head "$WAIT_URL" | grep ^HTTP | tr -s ' ' | cut -f 2 -d' ')
+      CODE=$(curl --silent --header "authorization: Bearer $TOKEN" --head "$WAIT_URL" | grep ^HTTP | tr -s ' ' | cut -f 2 -d' ')
       RETRIES=`expr $RETRIES + 1`
       echo "DEBUG. Waiting for $WAIT_URL = $WAIT_CODE. Retries $RETRIES, code $CODE"
     done
@@ -31,7 +31,7 @@ upload_zip_file()
       --retry 20 --retry-delay 0 --retry-max-time 40 --max-time 180 --limit-rate 5M \
       --request POST \
       --url $UPLOAD_URL?name=$FILE \
-      --header "Authorization: token $TOKEN" \
+      --header "authorization: Bearer $TOKEN" \
       --header "Content-Type: application/zip" \
       --data-binary @$FILE
 }
@@ -41,7 +41,7 @@ download_asset() {
   local RELEASE_TAG=$2
   local ASSET_NAME=$3
   local TOKEN=$4
-  local ASSET_URL=$($CURL_JSON $GH_API_REPO/releases/tags/$RELEASE_TAG | python3 -c \
+  local ASSET_URL=$(eval $CURL_JSON $GH_API_REPO/releases/tags/$RELEASE_TAG | python3 -c \
     "import sys, json; assets = [a for a in json.load(sys.stdin)['assets'] if a['name'] == '$ASSET_NAME']; print(assets[0]['browser_download_url']) if assets else ''" \
     )
   echo "INFO. asset url:"
@@ -64,7 +64,7 @@ download_file()
       --retry 20 --retry-delay 0 --retry-max-time 40 --max-time 180 --limit-rate 5M \
       --remote-header-name \
       --location \
-      --header "Authorization: token $TOKEN" \
+      --header "authorization: Bearer $TOKEN" \
       --header "Accept: application/octet-stream" \
       --url "$DOWNLOAD_URL" \
       --create-dirs \
