5ea5599341495aafe0cde4e5818879f72cd5a880
Dimitri Baron <dimitri.baron@rte-france.com>
2024-06-21 13:50:19 +0200

    #747 Missing environment variable
    Signed-off-by: Dimitri Baron <dimitri.baron@rte-france.com>

--- scripts/scriptsException.py
+++ scripts/scriptsException.py
@@ -1,3 +1,8 @@
 class UnknownBuildType(Exception):
     def __init__(self, buildtype_name):
         print("Unknown BuildType : {}".format(buildtype_name))
+
+
+class MissingEnvironmentVariable(Exception):
+    def __init__(self, env_var_name):
+        print("Missing environment variable : {}".format(env_var_name))
