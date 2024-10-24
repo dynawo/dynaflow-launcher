5ed90dbb4e37cde4d8c4d533a2cab82573b6c119
Dimitri Baron <dimitri.baron@rte-france.com>
2024-10-24 18:18:03 +0200

    #798 Update following DYN-2455
    Signed-off-by: Dimitri Baron <dimitri.baron@rte-france.com>

--- tests/algo/TestGeneratorsAlgo.cpp
+++ tests/algo/TestGeneratorsAlgo.cpp
@@ -83,7 +83,7 @@ class TestAlgoServiceManagerInterface : public DYN::ServiceManagerInterface {
   /**
    * @copydoc DYN::ServiceManagerInterface::getRegulatedBus
    */
-  boost::shared_ptr<DYN::BusInterface> getRegulatedBus(const std::string &regulatingComponent) const final { return boost::shared_ptr<DYN::BusInterface>(); }
+  std::shared_ptr<DYN::BusInterface> getRegulatedBus(const std::string &regulatingComponent) const final { return std::shared_ptr<DYN::BusInterface>(); }
 
  private:
   void updateSet(std::set<std::string> &set, const std::string &str, const std::string &vlid) const {
